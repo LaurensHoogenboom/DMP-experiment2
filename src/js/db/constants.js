7dbf2392a69cd8d57c1d954a90b53f82171c745e
LaurensH2000 <49717394+LaurensHoogenboom@users.noreply.github.com>
2021-10-26 16:46:32 +0200

    moved pressure descriptions

--- src/js/db/constants.js
+++ src/js/db/constants.js
@@ -1,1 +1,10 @@
 const pressureListStorageName = "pressureList";
+
+const pressureDescriptions = [
+    { type: "time", description: "Tijdsdruk" },
+    { type: "emotion", description: "Emotionele druk" },
+    { type: "archievement", description: "Prestatie druk" },
+    { type: "responsibility", description: "Verandwoordelijkheidsgevoel" },
+    { type: "adjustment", description: "Aanpassingsdruk" },
+    { type: "political", description: "Politieke druk" }
+];
