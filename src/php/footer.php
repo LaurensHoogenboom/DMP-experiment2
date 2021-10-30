b811c6e11dcd37ff4275601bc92016ddfbe246a6
Laurens <laurenshoogenboom2000@gmail.com>
2021-10-30 09:58:32 +0200

    some illustrations

--- src/php/footer.php
+++ src/php/footer.php
@@ -1,4 +1,6 @@
-</div>
+        </div>
+        <img src="/illustration.svg" alt="survey illustration" class="illustration">
+        <div class="floor"></div>
     </main>
 </body>
 </html>
--- src/php/header.php
+++ src/php/header.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/src/css/style.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+
     <title>Stress Reflectie Test</title>
 
     <script src="/src/js/jquery.js"></script>
@@ -16,13 +23,13 @@
     <script src="/src/js/db/controllers/pressure.js"></script>
     <script src="/src/js/db/controllers/moment.js"></script>
     <script src="/src/js/db/controllers/relevant.js"></script>
-    
+
     <script src="/src/js/modules/pressure.js"></script>
     <script src="/src/js/modules/moment.js"></script>
     <script src="/src/js/modules/values.js"></script>
     <script src="/src/js/modules/reflection.js"></script>
-    
 </head>
+
 <body>
     <main>
         <div id="form-wrapper">
