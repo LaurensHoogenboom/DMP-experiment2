7e5590c953f00ccfacf0a6ecbdab7997445aa62d
Laurens <laurenshoogenboom2000@gmail.com>
2021-11-05 15:06:09 +0100

    init

--- src/php/footer.php
+++ src/php/footer.php
@@ -2,5 +2,11 @@
         <img src="/illustration.svg" alt="survey illustration" class="illustration">
         <div class="floor"></div>
     </main>
+
+    <script>
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('/service-worker.js');
+        }
+    </script>
 </body>
 </html>
